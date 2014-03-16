78fce738b96433cda8ddfedb3c64c4950e443d69
Damien Guard <damien@envytech.co.uk>
2014-03-15 17:34:36 -0700

    Correct function keys.

--- keyboard/phantom/keymap_damieng.h
+++ keyboard/phantom/keymap_damieng.h
@@ -20,7 +20,7 @@
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty + a few media keys */
     KEYMAP(\
-        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,TRNS, PSCR,SLCK,BRK,  \
+        ESC, TRNS, F1, F2,  F3,  F4,  TRNS, F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PSCR,SLCK,BRK,  \
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,BSPC, INS, HOME,PGUP, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,ENT,       DEL, END, PGDN, \
         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NUHS,ENT,       MPLY,UP,  VOLU, \
